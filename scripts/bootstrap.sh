a91c0360140998dee12ea7f4f6f83de6cd508e95
Anwar Ishak <anwarishak@gmail.com>
2015-07-02 15:15:34 +0100

    Fixed so that sources.list not updated if already up to date

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -2,10 +2,16 @@
 
 echo ">>>>> Updating packages"
 
-echo "
+if grep -Fxq "deb http://us.archive.ubuntu.com/ubuntu/ trusty multiverse" /etc/apt/sources.list
+then
+    echo ">>>>> Sources list already up-to-date"
+else
+    echo ">>>>> Updating sources list"
+    echo "
 deb http://us.archive.ubuntu.com/ubuntu/ trusty multiverse
 deb-src http://us.archive.ubuntu.com/ubuntu/ trusty multiverse
 deb http://us.archive.ubuntu.com/ubuntu/ trusty-updates multiverse
 deb-src http://us.archive.ubuntu.com/ubuntu/ trusty-updates multiverse" >> /etc/apt/sources.list
+fi
 
 apt-get update -qq
